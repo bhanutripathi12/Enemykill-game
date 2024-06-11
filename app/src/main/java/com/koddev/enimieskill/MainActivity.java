14dc692cf9fc3aeb2479927a234cf2457dd79cf2
Asus <bhanu.tripathi12@gmail.com>
2024-06-11 19:10:46 +0530

    Final commit

---
+++ app/src/main/java/com/koddev/enimieskill/MainActivity.java
@@ -0,0 +1,26 @@
+package com.koddev.enimieskill;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SpaceShooter(this));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
